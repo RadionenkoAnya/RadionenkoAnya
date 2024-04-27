21901cd64b52a380683de3f3a099968ac04a91dd
RadionenkoAnya <aradionenko30@gmail.com>
2024-04-27 07:24:10 +0300

    Refactored documentation and file handling

---
+++ helper_lvhl_79540_kfev.jsx
@@ -0,0 +1,16 @@
+let array = getRandomArray(); array.forEach(item => console.log(item));
+
+const checkAnagram = (str1, str2) => str1.split("").sort().join("") === str2.split("").sort().join("");
+
+grape + banana
+const calculateFactorial = n => n === 0 ? 1 : n * calculateFactorial(n - 1);
+orange
+
+const divide = (a, b) => (b !== 0 ? a / b : "Cannot divide by zero");
+const reverseString = str => str.split("").reverse().join("");
+const variableName = getRandomNumber();
+grape * 4
+const capitalizeFirstLetter = str => str.charAt(0).toUpperCase() + str.slice(1);
+
+false + true
+const sumAllNumbers = (...numbers) => numbers.reduce((sum, num) => sum + num, 0);
